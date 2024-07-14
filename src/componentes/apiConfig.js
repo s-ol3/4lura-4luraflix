1b54867fda39cacf244c314161e9a4d761c2fb3f
sol3 <marzen25@gmail.com>
2024-07-13 21:51:54 -0300

    13.7.24 link mockapi en apiConfig

--- src/componentes/apiConfig.js
+++ src/componentes/apiConfig.js
@@ -1,5 +1,7 @@
 // cambiar a uno u otro para pruebas 
 
-//export const API_URL = 'http://localhost:3001'; 
+//export const API_URL = 'http://localhost:3001';  // funciona todo
 
-export const API_URL = 'https://4lura-4luraflix-api.vercel.app';
+//export const API_URL = 'https://4lura-4luraflix-api.vercel.app';  // funciona todo el crud pero requiere recarga de pagina a veces, o espera de unos minutos. los cambios son temporales
+
+export const API_URL = 'https://6692ca25346eeafcf46e3102.mockapi.io';    //-videos-/funciona add, edit, delete (agregué "" al id en json mockapi) , //-categorias-/ funciona add, edit, delete (agregando "" al id funciona todo) 
